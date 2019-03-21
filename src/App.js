4a73d9a595d56b1e48730adb35278b91fe1b48c4
elisamartin <elisamv93@gmail.com>
2019-03-21 16:03:26 +0100

    Added lotr data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+// import Users from './components/Users';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	// constructor(props) {
+	//   super(props);
+	//   this.state = {
+	//     users: []
+	//   };
+	// }
+
+	// componentDidMount() {
+	//   axios
+	//     .get('http://localhost:4444/api/users')
+	//     .then((response) => {
+	//       this.setState(() => ({ users: response.data }));
+	//     })
+	//     .catch((err) => console.log(err));
+	// }
+
+	render() {
+		return (
+			<div className="App">
+				<h2>LOTR React App</h2>
+				{/* <Users users={this.state.users} /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
